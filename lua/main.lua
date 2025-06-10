fa282466cd02a2c0bf21ef45ddda22a77d574254
Jesse Higginson <kadmium@gmail.com>
2025-06-10 06:32:18 +0000

    Fix some main.lua issues

--- lua/main.lua
+++ lua/main.lua
@@ -5,17 +5,21 @@ local Install = require "operations.install"
 -- Main execution logic
 local operation = reaper.GetExtState(Globals.SECTION, Globals.KEYS.operation)
 if not operation or operation == "" then
-    reaper.ShowConsoleMsg("No operation specified. Exiting.\n")
-    return
+    if reaper.GetExtState(Globals.SECTION, Globals.KEYS.script_action_id) == "" then
+        -- If the script is just called by Reaper without an operation and the project root folder is not set,
+        -- it's probably the initial installation request
+        Install()
+        reaper.ShowConsoleMsg("Installation completed.\n")
+        return
+    else
+        reaper.ShowConsoleMsg("No operation specified. Exiting.\n")
+        return
+    end
 end
 
 if Operations[operation] then
     Operations[operation]()
     reaper.DeleteExtState(Globals.SECTION, Globals.KEYS.operation, true)
-elseif reaper.GetExtState(Globals.SECTION, Globals.KEYS.script_action_id) == "" then
-    -- If the script is just called by Reaper without an operation and the project root folder is not set,
-    -- it's probably the initial installation request
-    Install()
 else
     reaper.ShowConsoleMsg("Unknown operation: " .. operation .. "\n")
 end
